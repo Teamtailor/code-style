e20ddc34b1e056a2b19bb8e99cb6077caaff40e1
Jonas Brusman <himynameisjonas@gmail.com>
2018-04-04 13:54:48 +0200

    Linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,39 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'ember'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended'
-  ],
+  plugins: ['prettier', 'ember'],
+  extends: ['eslint:recommended', 'plugin:ember/recommended', 'prettier'],
   env: {
-    browser: true
+    browser: true,
   },
   rules: {
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
+    'prettier/prettier': ['error', { trailingComma: 'es5', singleQuote: true }],
+    'generator-star-spacing': ['error', { before: false, after: false }],
+    'arrow-parens': [2, 'as-needed'],
+    'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    'max-statements-per-line': ['error', { max: 2 }],
+    'new-cap': 0,
+    camelcase: 'off',
+    'ember/named-functions-in-promises': 0,
+    'ember/no-on-calls-in-components': 0,
+    'ember/use-ember-get-and-set': 'error',
+    'ember/alias-model-in-controller': 0,
+    'no-useless-escape': 2,
+    'ember/no-old-shims': 2,
+    'ember/closure-actions': 1,
+    'ember-suave/require-access-in-comments': 0,
   },
   overrides: [
     // node files
@@ -23,16 +43,16 @@ module.exports = {
         'testem.js',
         'ember-cli-build.js',
         'config/**/*.js',
-        'lib/*/index.js'
+        'lib/*/index.js',
       ],
       parserOptions: {
         sourceType: 'script',
-        ecmaVersion: 2015
+        ecmaVersion: 2015,
       },
       env: {
         browser: false,
-        node: true
-      }
-    }
-  ]
+        node: true,
+      },
+    },
+  ],
 };
