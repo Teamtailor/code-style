3c41731c7d38ff5305c507bb0fd3909dfd2b0b7e
Jonas Brusman <himynameisjonas@gmail.com>
2018-04-05 15:56:18 +0200

    no html in toc

--- app/models/content.js
+++ app/models/content.js
@@ -1,8 +1,13 @@
 import DS from 'ember-data';
+import { computed, get } from '@ember/object';
 
 export default DS.Model.extend({
   language: DS.belongsTo('language', { async: true }),
 
   title: DS.attr('string'),
   html: DS.attr('string'),
+
+  strippedTitle: computed('title', function() {
+    return get(this, 'title').replace(/(<([^>]+)>)/gi, '');
+  }),
 });
